ca1a20b41e27532c78fbab327f7be3f3dbb7a8d3
hadirga <halexandergc@gmail.com>
2021-09-10 18:25:42 -0300

    Complete the Next.js blog tutorial using typescript

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+export default function Custom404() {
+  return (
+    <Layout>
+      <p>This is a custom 404 page</p>
+      <p>404 - Page not found!</p>
+    </Layout>
+  );
+}
