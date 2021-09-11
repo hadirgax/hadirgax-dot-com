b80ded235669d82f755976cb94e40baa067f1e6c
hadirga <halexandergc@gmail.com>
2021-09-11 01:09:00 -0300

    Add props to date component

--- components/date.tsx
+++ components/date.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { parseISO, format } from 'date-fns';
 
-export default function Date({ dateString }) {
+type DateProps = {
+  dateString: string;
+};
+
+export default function Date({ dateString }: DateProps) {
   const date = parseISO(dateString);
   return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
 }
